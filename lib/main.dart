09d41cc0f65de4a8e5b2739507c0134a224ec287
EtorArza <etorarza@gmail.com>
2020-12-27 23:04:12 +0100

    open with android intent

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:notes/services/sharedPref.dart';
 import 'screens/home.dart';
 import 'data/theme.dart';
 import 'screens/settings.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -15,10 +17,38 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   Settings settings;
+  StreamSubscription _intentDataStreamSubscription;
+  List<SharedMediaFile> _sharedFiles;
+  String _sharedText;
+
   @override
   void initState() {
     super.initState();
     settings = Settings();
+
+    // For sharing or opening urls/text coming from outside the app while the app is in the memory
+    _intentDataStreamSubscription = ReceiveSharingIntent.getTextStream().listen((String value) {
+      setState(() {
+        _sharedText = value;
+        print("Shared: $_sharedText");
+      });
+    }, onError: (err) {
+      print("getLinkStream error: $err");
+    });
+
+    // For sharing or opening urls/text coming from outside the app while the app is closed
+    ReceiveSharingIntent.getInitialText().then((String value) {
+      setState(() {
+        _sharedText = value;
+        print("Shared: $_sharedText");
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _intentDataStreamSubscription.cancel();
+    super.dispose();
   }
 
   @override
