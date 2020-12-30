8ba0a87767a8df65f7730b496eec4773526ee921
EtorArza <etorarza@gmail.com>
2020-12-30 13:44:36 +0100

    open in frek working

--- android/app/src/main/java/com/etorarza/frek/MainActivity.java
+++ android/app/src/main/java/com/etorarza/frek/MainActivity.java
@@ -1,8 +1,12 @@
 package com.etorarza.frek;
 
 
+import java.io.*;
+import java.net.URI;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.net.Uri;
 
 import androidx.annotation.NonNull;
 
@@ -11,9 +15,17 @@ import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+
+
+
+
+
+
+
+
 public class MainActivity extends FlutterActivity {
 
-  private String sharedText;
+  private String sharedText = "";
   private static final String CHANNEL = "app.channel.shared.data";
 
   @Override
@@ -22,13 +34,54 @@ public class MainActivity extends FlutterActivity {
     Intent intent = getIntent();
     String action = intent.getAction();
     String type = intent.getType();
+    String data = intent.getDataString();
+    Uri uri = intent.getData();
+    String path = "null";
 
-    if (Intent.ACTION_SEND.equals(action) && type != null) {
-      if ("text/plain".equals(type)) {
-        handleSendText(intent); // Handle text being sent
-      }
+    if (action == null) {
+      action = "null";
     }
+    if (type == null) {
+      type = "null";
+    }
+    if (data == null) {
+      data = "null";
+    }
+    if (uri != null) {
+      path = uri.getPath();
+    }
+
+
+    sharedText+= "action: " + action + " | ";
+    sharedText+= "type: " +  type + " | ";
+    sharedText+= "data: " +  data + " | ";
+    sharedText+= "path: " +  path + " | ";
+
+    if (Intent.ACTION_SEND.equals(action) || Intent.ACTION_VIEW.equals(action)) {
+
+      sharedText += "intent match | ";
+      
+      try {
+        // https://stackoverflow.com/questions/31069556/android-read-text-file-from-uri
+        InputStream in = getContentResolver().openInputStream(uri);
+
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(in));
+        StringBuilder total = new StringBuilder();
+        for (String line; (line = r.readLine()) != null; ) {
+            total.append(line).append('\n');
+        }
+        String content = total.toString();
+        sharedText += "read_content:" + content + "|";
+
+
+    }catch (Exception e) {
+        sharedText += "error on read: " + e.toString() + "|";
+    }
+
+     }
   }
+  
 
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
@@ -46,6 +99,9 @@ public class MainActivity extends FlutterActivity {
   }
 
   void handleSendText(Intent intent) {
-    sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+    //sharedText += intent.getStringExtra(Intent.EXTRA_TEXT);
   }
+
+
+
 }
