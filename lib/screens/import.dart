9d95f8912a768bb75d717b0abe446cb0ad84bec6
Etor Arza <etorarza@gmail.com>
2021-01-09 22:41:00 +0100

    move backup to import.dart and get rid of file_picker

--- lib/screens/import.dart
+++ lib/screens/import.dart
@@ -43,12 +43,15 @@ class ImportScreenState extends State<ImportScreen> with TickerProviderStateMixi
   bool _backupJustDone = false;
   bool _corruptedFileErrorDuringBackup = false;
   double _backupProgress = 0.0;
+  bool _backupRestoreMode = false;
+  bool _checkboxConfirmed = false;
 
   @override
   void initState() {
     currentNote = null;
     super.initState();
     NotesDatabaseService.db.init();
+    _backupRestoreMode = false;
     print("Imported type in initState import.dart: ${this.widget.importedType}");
     switch (this.widget.importedType) {
       case "FrekDB":
@@ -77,7 +80,7 @@ class ImportScreenState extends State<ImportScreen> with TickerProviderStateMixi
   }
 
   void handleImportFrekDB() async {
-    restoreBackup();
+    _backupRestoreMode = true;
   }
 
   void handleImportFrekCard() async {
@@ -123,59 +126,124 @@ class ImportScreenState extends State<ImportScreen> with TickerProviderStateMixi
     });
   }
 
-  void restoreBackup() async {
+  Future<void> restoreBackup() async {
     allowExitSettings = false;
+    setState(() {
+      _showBackupProgress = true;
+    });
+
     await NotesDatabaseService.db.restoreBackup(this, context, this.widget.homePageState);
-    allowExitSettings = true;
   }
 
   Widget buildImportFrekDB(BuildContext context) {
-    print("handleImportFrekDB");
-
-    return WillPopScope(
-        onWillPop: () async {
-          return allowExitSettings;
-        },
-        child: Scaffold(
-          body: ListView(
-            physics: BouncingScrollPhysics(),
-            children: <Widget>[Container(child: this._getProgressBar())],
-          ),
-        ));
-
-    print("buildImportFrekDB");
-    return Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          height: 25,
-        ),
-        backButton(context),
-        Spacer(),
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+    return _showBackupProgress
+        ? WillPopScope(
+            onWillPop: () async {
+              return allowExitSettings;
+            },
+            child: Scaffold(
+              body: ListView(
+                physics: BouncingScrollPhysics(),
+                children: <Widget>[Container(child: this._getProgressBar())],
+              ),
+            ))
+        : Scaffold(
+            body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Text(
-                    'This is a backup file. You can restore it from settings.',
-                    style: TextStyle(fontFamily: 'ZillaSlab', color: Theme.of(context).primaryColor, fontSize: 25),
-                  )),
-              Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Text(
-                    "If you do, all your data will be replaced. To avoid unintentionally deleting all your data, you need to go to settings to restore the backup. By forcing the user to first go settings and then select the backup file, we make sure that the user knows what it is doing (replacing all its data with the backup's content).",
-                    style: TextStyle(fontFamily: 'ZillaSlab', color: Colors.grey[400], fontSize: 16),
-                  )),
+              Container(
+                height: 25,
+              ),
+              backButton(context),
+              Spacer(),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Text(
+                          'This is a backup file.',
+                          style: TextStyle(fontFamily: 'ZillaSlab', color: Theme.of(context).primaryColor, fontSize: 25),
+                        )),
+                    Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Text(
+                          "You can restore it. If you do, all your data will be irreversibly replaced.",
+                          style: TextStyle(fontFamily: 'ZillaSlab', color: Colors.grey[400], fontSize: 16),
+                        )),
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Row(
+                        children: [
+                          Text(
+                            "I understand what I am doing",
+                            style: TextStyle(
+                              fontFamily: 'ZillaSlab',
+                              color: Colors.grey[400],
+                              fontSize: 16,
+                            ),
+                            overflow: TextOverflow.visible,
+                          ),
+                          Checkbox(
+                              value: _checkboxConfirmed,
+                              onChanged: (value) {
+                                setState(() {
+                                  _checkboxConfirmed = value;
+                                });
+                              }),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Container(
+                              padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
+                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.0), color: Colors.grey[600], border: Border.all()),
+                              child: Text(
+                                'cancel'.toUpperCase(),
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () async {
+                            if (_checkboxConfirmed && _backupRestoreMode) {
+                              await restoreBackup();
+                            }
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Container(
+                              padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 10.0),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  color: _checkboxConfirmed ? Colors.blueAccent : Colors.grey[600],
+                                  border: Border.all()),
+                              child: Text(
+                                'restore'.toUpperCase(),
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
             ],
-          ),
-        ),
-        Spacer(),
-      ],
-    ));
+          ));
   }
 
   Widget buildImportFrekCard(BuildContext context) {
@@ -421,6 +489,7 @@ class ImportScreenState extends State<ImportScreen> with TickerProviderStateMixi
     });
 
     Future.delayed(Duration(milliseconds: nMiliSecondsDoneInScreen + nMilSecondsLastPartProgress), () {
+      allowExitSettings = true;
       Navigator.pop(context);
     });
 
@@ -452,6 +521,7 @@ class ImportScreenState extends State<ImportScreen> with TickerProviderStateMixi
     });
 
     Future.delayed(Duration(milliseconds: nMiliSecondsDoneInScreen + nMilSecondsLastPartProgress), () {
+      allowExitSettings = true;
       Navigator.pop(context);
     });
   }
